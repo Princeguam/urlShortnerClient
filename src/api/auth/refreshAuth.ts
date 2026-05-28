d921d6234aa0d850a135a880c076a8fe355737bd
Okogbule Goodluck <okogbulegoodluck@gmail.com>
2026-05-28 16:42:24 +0100

    rapid fix #02

--- src/api/auth/refreshAuth.ts
+++ src/api/auth/refreshAuth.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { setAccessToken } from "./tokenStorage";
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
+const BASE_URL = import.meta.env.VITE_APIV1_URL || "http://localhost:3000";
 
 interface RefreshResponse {
     accessToken: string;
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
-
+    plugins: [react(), tailwindcss()],
+    server: {
+        port: 4000, // Sets the desired port (e.g., 3000)
+        strictPort: true, // Forces Vite to fail if the port is already in use
+    },
+});
 
 // import { defineConfig } from 'vite'
 // import tailwindcss from '@tailwindcss/vite'
